6afee22bde3d88c5a8ab8701e57d076c98300239
GabrielFerrin <gabrielferrin@byui.edu>
2024-07-16 21:38:06 -0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { DataProvider } from './context/DataProvider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className="theme">
-    <QueryClientProvider client={queryClient}>
-      <DataProvider>
-        <App />
-      </DataProvider>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <DataProvider>
+          <App />
+        </DataProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </div>
 )
